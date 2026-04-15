2f396261dd96a3855cf77509eb5b3357509bcd04
Forzun <vbhavesh219@gmail.com>
2026-04-16 02:55:05 +0530

    adding circle thing

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,16 @@
 "use client"
+import { stat } from "fs/promises"
 import { useEffect, useRef, useState } from "react"
+
+type Shape = "rect" | "pencil" | "circle"
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null)
-  const [color, setColor] = useState<string>("black")
+  const [color, setColor] = useState<string>("white")
   const startX = useRef(0)
   const startY = useRef(0)
   const [isMouseDown, setMouseDown] = useState<boolean>(false)
+  const [shape, setShape] = useState<Shape>("circle")
 
   useEffect(() => {
     const canvas = canvasRef.current
@@ -14,10 +18,9 @@ export default function Page() {
     const ctx = canvas.getContext("2d")
     if (ctx == null) return
     ctx.fillRect(0, 0, canvas.width, canvas.height)
-    ctx.strokeStyle = "white"
+    ctx.strokeStyle = color
 
     ctx.lineWidth = 1
-    ctx.stroke()
   }, [color])
 
   const onMouseDwon = (event: React.MouseEvent<HTMLCanvasElement>) => {
@@ -31,8 +34,6 @@ export default function Page() {
     setMouseDown(true)
     startX.current = x
     startY.current = y
-    const ctx = canvas.getContext("2d")
-    if (ctx == null) return
   }
 
   const onMouseUp = (event: React.MouseEvent<HTMLCanvasElement>) => {
@@ -55,13 +56,28 @@ export default function Page() {
     const y = event.clientY - rect.top
     const width = x - startX.current
     const height = y - startY.current
+
     const ctx = canvas.getContext("2d")
     if (ctx == null) return
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.beginPath()
-    ctx.rect(startX.current, startY.current, width, height)
-    ctx.fillStyle = "white"
-    ctx.stroke()
+
+    ctx.fillStyle = "black"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    ctx.strokeStyle = color
+    ctx.lineWidth = 1
+
+    if (shape == "circle") {
+      const dx = x - startX.current
+      const dy = y - startY.current
+      const radius = Math.sqrt(dx * dx + dy * dy)
+      ctx.beginPath()
+      ctx.arc(startX.current, startY.current, radius, 0, 2 * Math.PI, true)
+      ctx.stroke()
+    } else {
+      ctx.beginPath()
+      ctx.rect(startX.current, startY.current, width, height)
+      ctx.stroke()
+    }
   }
 
   return (
